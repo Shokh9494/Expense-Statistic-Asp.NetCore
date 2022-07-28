deace4f7067fae7600cd299eaeb5da1432a72762
Shoxrux <wox1686482@gmail.com>
2022-07-28 22:13:00 +0500

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Statistic.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName="nvarchar(50)")]
+        public string Titel { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Type { get; set; } = "Expense";   
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Expense_Statistic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
